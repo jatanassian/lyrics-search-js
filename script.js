ad60f8b18de01eaf5191418d2d8da6b71d2fce61
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-31 10:25:02 -0400

    display the result of the fetch in the DOM using forEach

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
-const form = document.getElementById('form');
-const search = document.getElementById('search');
-const result = document.getElementById('result');
-const more = document.getElementById('more');
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const result = document.getElementById("result");
+const more = document.getElementById("more");
 
-const apiURL = 'https://api.lyrics.ovh';
+const apiURL = "https://api.lyrics.ovh";
 
 // Search by song or artist
 async function searchSongs(term) {
-//   fetch(`${apiURL}/suggest/${term}`)
-//     .then(res => res.json())
-//     .then(data => console.log(data))
+  //   fetch(`${apiURL}/suggest/${term}`)
+  //     .then(res => res.json())
+  //     .then(data => console.log(data))
 
   const res = await fetch(`${apiURL}/suggest/${term}`);
-  const data = res.json();
+  const data = await res.json();
 
   showData(data);
 }
 
 // Display song and artist in DOM
 function showData(data) {
-  
+  let output = "";
+
+  data.data.forEach(song => {
+    output += `
+      <li>
+        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+      </li>
+    `;
+  });
+
+  result.innerHTML = `
+    <ul class="songs">
+      ${output}
+    </ul>
+  `;
 }
 
 // Event listeners
-form.addEventListener('submit', e => {
+form.addEventListener("submit", e => {
   e.preventDefault();
 
   const searchTerm = search.value.trim();
 
   if (!searchTerm) {
-    alert('Please type in a search term');
+    alert("Please type in a search term");
   } else {
     searchSongs(searchTerm);
   }
