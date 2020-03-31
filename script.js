9eab09b2d988b0be94058ff4a6623eacd5789596
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-31 10:44:34 -0400

    add the next and previous buttons to display more songs if they exist

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const form = document.getElementById("form");
-const search = document.getElementById("search");
-const result = document.getElementById("result");
-const more = document.getElementById("more");
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
 
-const apiURL = "https://api.lyrics.ovh";
+const apiURL = 'https://api.lyrics.ovh';
 
 // Search by song or artist
 async function searchSongs(term) {
@@ -47,19 +47,44 @@ function showData(data) {
             <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
           </li>`
         )
-        .join("")}
+        .join('')}
     </ul>
   `;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+      ${
+        data.prev
+          ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+          : ''
+      }
+      ${
+        data.next
+          ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+          : ''
+      }
+    `;
+  } else {
+    more.innerHTML = '';
+  }
+}
+
+// Get prev and next songs
+async function getMoreSongs(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  showData(data);
 }
 
 // Event listeners
-form.addEventListener("submit", e => {
+form.addEventListener('submit', e => {
   e.preventDefault();
 
   const searchTerm = search.value.trim();
 
   if (!searchTerm) {
-    alert("Please type in a search term");
+    alert('Please type in a search term');
   } else {
     searchSongs(searchTerm);
   }
